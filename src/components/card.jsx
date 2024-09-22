3e52e6c17c083eab10fc622ca807cd563f29b408
Nishchal Namdeo <nishchal04namdeo@gmail.com>
2024-09-22 14:22:52 +0530

    conditional rendering

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -8,6 +8,7 @@ function Card() {
       name: "amazon",
       description:
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam at aliquid nemo",
+      instock: true,
     },
     {
       image:
@@ -15,14 +16,21 @@ function Card() {
       name: "object",
 
       description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+      instock: true,
     },
     {
       image:
         "https://plus.unsplash.com/premium_photo-1685134731677-1f5021c1f764?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       name: "News",
       description: "Lorem, ipsum dolor sit amet .",
+      instock: false,
     },
   ];
+
+  const handleClickDownload = () => {
+    alert("chal raha hai");
+  };
+
   return (
     <div className=" w-full h-screen flex items-center justify-center gap-10 bg-gray-300">
       {data.map((elem, index) => (
@@ -38,6 +46,14 @@ function Card() {
           <div className="w-full px-3 py-4">
             <h2 className="text-xl font-bold">{elem.name}</h2>
             <p className="text-xs mt-3">{elem.description}</p>
+            <button
+              onClick={handleClickDownload}
+              className={`px-6 py-2 ${
+                elem.instock ? "bg-blue-500" : "bg-red-600"
+              } mt-2 rounded`}
+            >
+              {elem.instock ? "instock" : "out of stock"}
+            </button>
           </div>
         </div>
       ))}
